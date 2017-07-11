50ab1f8822086e523c5a97a62528267fbf31a5e6
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 15:49:56 +0000

    Generated app and updated readme.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,3 @@ class ApplicationController < ActionController::Base
     @copyright = DevcampViewTool::Renderer.copyright 'Collin Carlson', 'All rights reserved'
   end
 end
-
-module DevcampViewTool
-  class Renderer
-    def self.copyright name, msg
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
-end
