b3be0b9d9297905e90a620afa9d8e739a3f9e47c
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 14:30:34 +0000

    When I Strugled.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,11 @@ class ApplicationController < ActionController::Base
   before_action :set_copyright
   
   def set_copyright
-    @copyright = DevcampViewTool::Renderer.copyright 'Collin Carlson', 'All rights reserved'
+    @copyright = CollinViewTool::Renderer.copyright 'Collin Carlson', 'All rights reserved'
   end
 end
 
-module DevcampViewTool
+module CollinViewTool
   class Renderer
     def self.copyright name, msg
       "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
