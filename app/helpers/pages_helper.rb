7680128ff5b8397fe3459d6cf96823c0d3bbcf95
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 15:10:13 +0000

    Built twitter parser for tweet links.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,28 @@
 module PagesHelper
+  def twitter_parser tweet
+    tweet = "NatsuTravel: Day45:最後の演習となりました。。。今度こそバシッと決めてやりたい！　#Rails #Progate #100DaysOfCode https://t.co/18tfIhLrO1"
+    
+        regex = %r{
+      \b
+      (
+        (?: [a-z][\w-]+:
+         (?: /{1,3} | [a-z0-9%] ) |
+          www\d{0,3}[.] |
+          [a-z0-9.\-]+[.][a-z]{2,4}/
+        )
+        (?:
+         [^\s()<>]+ | \(([^\s()<>]+|(\([^\s()<>]+\)))*\)
+        )+
+        (?:
+          \(([^\s()<>]+|(\([^\s()<>]+\)))*\) |
+          [^\s`!()\[\]{};:'".,<>?«»“”‘’]
+        )
+      )
+    }ix
+    
+     tweet.gsub(regex) do |url|
+      "<a href='#{url}' target='_blank'>#{url}</a>"
+    end.html_safe
+  end
 end
+
